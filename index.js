7d036f52e85ea6ff384bf85121943c7d30542ab8
abul kalam <bd.abulkalam800@gamil.com>
2025-06-03 15:37:18 +0300

    added job post api

--- index.js
+++ index.js
@@ -24,32 +24,64 @@ async function run() {
     await client.connect();
     // database and collection
     const jobsCollection = client.db("Jop_portal").collection("jobs");
-    const applicationCollectin =  client.db("Jop_portal").collection("applayList")
-//  application applay api 
-    app.post('/applications',async(req,res)=>{
+    const applicationCollectin = client
+      .db("Jop_portal")
+      .collection("applayList");
+
+    app.get("/applications/job/:job_id", async(req,res)=>{
+      const job_id= req.params.job_id;
+      console.log(job_id)
+      const  query = {jobId: job_id}
+      const result = await applicationCollectin.find(query).toArray();
+      res.send(result)
+    });
+    //  application applay api
+    app.post("/applications", async (req, res) => {
       const application = req.body;
       const result = await applicationCollectin.insertOne(application);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    // applicationList api 
-    app.get('/applications',async(req,res)=>{
-      const email = req.query.email
-      const query ={applicant:email}
-      const result = await applicationCollectin.find(query).toArray()
-      res.send(result)
-    })
-    //  jobs api
+    // applicationList api
+    app.get("/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { applicant: email };
+      const result = await applicationCollectin.find(query).toArray();
+      // bad way to aggregate data
+      for (const application of result) {
+        const jobId = application.jobId;
+        const jobQuery = { _id: new ObjectId(jobId) };
+        const job = await jobsCollection.findOne(jobQuery);
+        application.company = job.company;
+        application.title = job.title;
+        application.company_logo = job.company_logo;
+        application.jobType = job.jobType;
+        application.status = job.status;
+      }
+      res.send(result);
+    });
+    //  jobs get api
     app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.hr_email = email;
+      }
+      const cursor = jobsCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
-  //  jobs api detaiels 
+    //  jobs api specefiq
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+    //  job post api
+    app.post("/jobs", async (req, res) => {
+      const jobsPost = req.body;
+      const result = await jobsCollection.insertOne(jobsPost);
       res.send(result);
     });
 
